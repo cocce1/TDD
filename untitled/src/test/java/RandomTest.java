0634b14813a3b8cdf6c49a1c97d3855da1ccc714
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-10-26 14:44:45 +0200

    second commit

---
+++ untitled/src/test/java/RandomTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RandomTest {
+
+    @Test
+    @DisplayName("Nu finns en test")
+    void random_test(){
+        
+    }
+}
