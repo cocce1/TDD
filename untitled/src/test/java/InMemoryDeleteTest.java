9194888876e1008e2eb48c60f47cada1e8807fe2
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-10-27 14:23:57 +0200

    green light on right error message when remove a question

--- untitled/src/test/java/InMemoryDeleteTest.java
+++ untitled/src/test/java/InMemoryDeleteTest.java
@@ -3,13 +3,20 @@ import org.example.Questions;
 import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 
 public class InMemoryDeleteTest {
 
     InMemory memory;
+    Map<Integer,Questions> questionsMap;
     @BeforeEach
     public void setup(){
         memory = new InMemory();
+        questionsMap =  new HashMap<>();
     }
 
     @Test
@@ -24,4 +31,17 @@ public class InMemoryDeleteTest {
 
     Assertions.assertEquals(expected,result);
 }
+
+   @Test
+    @DisplayName("Error message if there is no question with that id")
+    void error_message_if_no_match(){
+       Questions questions = new Questions(1,"Hallo",new String[]{"1","2","3"},"4");
+       InMemory mock = Mockito.mock(InMemory.class);
+       Mockito.when(mock.getQuestion(questions.getId())).thenReturn(questions);
+       String result =  memory.delete(questions.getId());
+
+       String expected = "can't find Question with id: " + 1;
+
+       Assertions.assertEquals(expected,result);
+   }
 }
---
+++ untitled/src/test/java/inMemoryUpdateTest.java
@@ -0,0 +1,2 @@
+public class inMemoryUpdateTest {
+}
